04a64cc6bb7e28a0a79da6d56262477c5cec5e3e
Bautista Garcia <bautyroccogarcia@gmail.com>
2023-05-30 23:27:53 -0300

    feat(head): added logo and page name

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import CardsCarousel from './components/imageCarousel'
 import Banner from './components/banner'
 export default function Home() {
   return (
-    <main className='w-screen h-screen'>
-      <Header />
-      <Banner />
-      <FeaturesCards />
-      <CardsCarousel />
-    </main>
+    <div>
+      <link rel="icon" href="/logo.ico" />
+      <main className='w-screen h-screen'>
+        <Header />
+        <Banner />
+        <FeaturesCards />
+        <CardsCarousel />
+      </main>
+    </div>
   )
 }
